fd644d14be8a7b0a88bf23ca9794d9227c75163b
mani <srimanikantavsa@gmail.com>
2025-05-01 23:21:22 +0000

    changes added

--- 02-variable.sh
+++ 02-variable.sh
@@ -4,6 +4,6 @@ person1=chintu
 person2=mani
 
 
-echo "$person1:: How are you $person2"
+echo "${person1}:: How are you $person2"
 echo "$person1:: I am fine what about you"
 
--- 03-variable.sh
+++ 03-variable.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 
+
+
+
+
+person1=$1
+person2=$2
+
+
+ #Note:- Here u are just giving values, Not even you are giving any names
+
+
+
+echo "$person1:: How are you $person2"
+echo "$person1:: I am fine what about you"
+
